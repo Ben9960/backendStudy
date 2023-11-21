d183d0f0be3a4d2b424e6bd22ce46d9e68430a8a
InBeomJin <ben9269960@gmail.com>
2023-11-21 09:16:40 +0900

    root

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 'use strict';
+import express from 'express';
+import bodyParser from 'body-parser'; // 뒤에거가 실제 package이름
+import cors from 'cors'; //데이터 베이스에 접근하는 거/ 연산하는 거 따로 / etc ...
 
-const WebSocket = require('./lib/websocket');
+const serverPort = 3000;
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extends:false}));
+app.use(cors());
 
-WebSocket.createWebSocketStream = require('./lib/stream');
-WebSocket.Server = require('./lib/websocket-server');
-WebSocket.Receiver = require('./lib/receiver');
-WebSocket.Sender = require('./lib/sender');
+const router = express.Router(); // 라우터는 추가적인 경로를 정해줌  http://172.23.35.32/api/v1/ping --> 숫자 뒤에 붙은것들
 
-WebSocket.WebSocket = WebSocket;
-WebSocket.WebSocketServer = WebSocket.Server;
+const ping = (req, res) =>{
+    const result = "pong";
+    res.send({result});
+    console.log("ping is called");
+}
 
-module.exports = WebSocket;
+router.route('/ping').get(ping); 
+app.use('/test/api/v1', router);
+app.listen(serverPort);
+
+
+
